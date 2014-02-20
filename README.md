9756762a43bfcf863c3992a46ecfe29f91771a5a
Nechtan <dnechtan@gmail.com>
2014-02-19 22:41:08 -0300

    v1.0.2 with options

--- README.md
+++ README.md
@@ -1,16 +1,48 @@
-## Raj v.1.0.1 - RoadAppsJavascript
+## Raj v.1.0.2 - RoadAppsJavascript
 ---
 
 #### Fast and lightweight Javascript async loader with preloader.
 
 
 
-**Example**:
+**Simple example**:
 
 ```javascript
 Raj(['assets/1.js', 'assets/2.js', 'assets/something.js'], function() {
   console.log('hell done');
 });
+```
+
+**With config**
+
+```javascript
+Raj({
+	baseUrl: 'http://localhost/',
+	paths: {
+		a: 'assets',
+		lib: 'assets/js/libs'
+	}
+},
+['lib/underscore.js', 'a/1.js', 'a/2.js', 'a/something.js'], function() {
+	// 'lib/underscore.js' === 'http://localhost/assets/js/libs/underscore.js'
+	console.log('hell done');
+});
+
+// pre-config
+
+Raj.config = { baseUrl: '/assets/js' };
+
+Raj(['jquery.js', 'bootstrap.js'], function() { console.log('ilovejs'); });
+
+```
+
+##### Using with bower
+
+**Installing**
+```bash
+$ sudo npm -g i bower
+$ cd your-project
+$ bower install raj
 ```
 
 *Copyright (C) 2014, RoadApps.net*
--- Raj.js
+++ Raj.js
@@ -1,5 +1,5 @@
 /**
- * Raj v1.0.1 - RoadApps Async Javascript Loader
+ * Raj v1.0.2 - RoadApps Async Javascript Loader
  * @license Copyright (c) 2013-2014, Road Apps All Rights Reserved.
  * Available via the MIT.
  * see: http://github.com/roadapps for details
@@ -9,27 +9,49 @@
 /*global require: false, exports: false */
 
 var Raj = function Raj(rsrc, cb) {
+
+  var options = Raj.config || {
+    baseUrl: '',
+    paths: {}
+  };
+
+  // calling with config
+  if(3 === arguments.length && typeof rsrc === 'object') {
+    options.baseUrl = rsrc['baseUrl'] || '';
+    options.paths = rsrc['paths'] || {};
+    rsrc = cb;
+    cb = arguments[2];
+  }
+
   var i = 0, c, a, t = 0,
       m = rsrc.length, k = 'script',
       e = 'addEventListener',
-      b = function(err, src) {
-        if(++t >= m) 
-        	cb();
-      	else
-      		L(rsrc[++i]);
+      b = function(err, src) { // callback
+        if(++t >= m)
+          cb();
+        else
+          L(rsrc[++i]);
       },
-      L = function(j) {
-		    var c = document.getElementsByTagName(k)[0];
-		    		a = document.createElement(k);
-		    a.src = j;
-		    a[e] ? (
-		        a[e]("load", b.bind(this, null, j), !1), 
-		        a[e]("error", b.bind(this, true, j), !1)
-		      ) :
-		      a.onreadystatechange = function() {
-		        a.readyState in {loaded:1, complete:1} && (a.onreadystatechange = null, b(j));
-		      };
-		    c.parentNode.insertBefore(a, c);
+      R = function(src) { // parse src
+        var r = src.split('/');
+        (typeof options.paths[r[0]] === 'string') &&
+          (r[0] = options.paths[r[0]]);
+
+        console.log(r);
+        return ''.concat(options.baseUrl, r.join('/'))
+      },
+      L = function(j) { // loader
+        var c = document.getElementsByTagName(k)[0];
+            a = document.createElement(k);
+        a.src = R(j);
+        a[e] ? (
+            a[e]("load", b.bind(this, null, R(j)), !1),
+            a[e]("error", b.bind(this, true, R(j)), !1)
+          ) :
+          a.onreadystatechange = function() {
+            a.readyState in {loaded:1, complete:1} && (a.onreadystatechange = null, b(R(j)));
+          };
+        c.parentNode.insertBefore(a, c);
       }
-      L(rsrc[i]);
+      L(rsrc[i]); // let the Raj begin
 }
